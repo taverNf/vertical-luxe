e90e4cf1ce3ec47a831bf49d3909b3ac87f177c7
taverNf <ntavernarofeliciano@gmail.com>
2025-04-28 20:08:54 -0300

    0428 - logica dos tamanhos

--- src/js/buy_page.js
+++ src/js/buy_page.js
@@ -2,32 +2,38 @@ const produtoJSON = sessionStorage.getItem('productSelected')
 const produto = JSON.parse(produtoJSON) // Transforma a string dos detalhes de volta para um objeto
 
 const img = document.getElementById('productImg')
-img.src = produto.img
+    img.src = produto.img
 
 const productName = document.getElementById('productName')
-productName.textContent = produto.nome
+    productName.textContent = produto.nome
 
 const price = document.getElementById('price')
-const priceFormated = produto.price.toFixed(2)
-price.textContent = `R$ ${priceFormated} no pix`
+    const priceFormated = produto.price.toFixed(2)
+    price.textContent = `R$ ${priceFormated} no pix`
 const parcel = document.getElementById('parcel')
-const parcelPrice = priceFormated / 3
-parcel.textContent = `ou parcele em até 3x de ${parcelPrice.toFixed(2)} sem juros`
+    const parcelPrice = priceFormated / 3
+    parcel.textContent = `ou parcele em até 3x de ${parcelPrice.toFixed(2)} sem juros`
 
 const productColor = document.getElementById('productColor')
-const colorString = produto.colorString
-productColor.innerHTML = `<b>Cor do produto: </b> ${colorString}`
+    const colorString = produto.colorString
+    productColor.innerHTML = `<b>Cor do produto: </b> ${colorString}`
 const color = document.getElementById('color')
-color.style.backgroundColor = produto.color
+    color.style.backgroundColor = produto.color
 
-for (let i = 0; i < produto.sizes.length; i++){
+
+const sizeBase = [38, 39, 40, 41, 42, 43, 44]
+for (let i = 0; i < sizeBase.length; i++){
     let size = document.createElement('span')
     size.className = 'size'
-    size.textContent = produto.sizes[i]
+    size.textContent = sizeBase[i]
+
+    if(produto.sizes.includes(sizeBase[i])){
+        size.classList.add('contain')
+    }
 
     const sizeGrid = document.getElementById('sizeGrid')
     sizeGrid.appendChild(size)
 }
 
 const nomeProduto = produto.nome
-document.title = `${nomeProduto} | Loja exemplo`
+    document.title = `${nomeProduto} | Loja exemplo`
