4c2daefc0b0d179d151335279305f1ef9fb842cd
taverNf <ntavernarofeliciano@gmail.com>
2025-05-01 14:09:28 -0300

    0501 - logica sizeselected+start buypage

--- src/js/product_page.js
+++ src/js/product_page.js
@@ -1,12 +1,17 @@
 const produtoJSON = sessionStorage.getItem('productSelected')
 const produto = JSON.parse(produtoJSON) // Transforma a string dos detalhes de volta para um objeto
 
+// Imagem do produto
 const img = document.getElementById('productImg')
     img.src = produto.img
 
+
+// Nome do produto
 const productName = document.getElementById('productName')
     productName.textContent = produto.nome
 
+
+// Preço do produto
 const price = document.getElementById('price')
     const priceFormated = produto.price.toFixed(2)
     price.textContent = `R$ ${priceFormated} no pix`
@@ -14,6 +19,8 @@ const parcel = document.getElementById('parcel')
     const parcelPrice = priceFormated / 3
     parcel.textContent = `ou parcele em até 3x de ${parcelPrice.toFixed(2)} sem juros`
 
+
+// Cor do produto
 const productColor = document.getElementById('productColor')
     const colorString = produto.colorString
     productColor.innerHTML = `<b>Cor do produto: </b> ${colorString}`
@@ -21,6 +28,8 @@ const color = document.getElementById('color')
     color.style.backgroundColor = produto.color
 
 
+// Tamanhos do produto
+let sizeSelected
 const sizeBase = [38, 39, 40, 41, 42, 43, 44]
 for (let i = 0; i < sizeBase.length; i++){
     let size = document.createElement('span')
@@ -29,11 +38,37 @@ for (let i = 0; i < sizeBase.length; i++){
 
     if(produto.sizes.includes(sizeBase[i])){
         size.classList.add('contain')
+        size.onclick = () => {
+            // Seleciona todos elementos e remove a classe selecionada
+            document.querySelectorAll('.contain.selected').forEach(item =>
+                {item.classList.remove('selected')})
+
+            sizeSelected = sizeBase[i]
+            size.classList.add('selected')
+
+            document.getElementById('productSize').innerHTML = `<b>Tamanho do produto: </b> ${sizeSelected}`
+        }
     }
 
     const sizeGrid = document.getElementById('sizeGrid')
     sizeGrid.appendChild(size)
 }
 
+
+// Título do site
 const nomeProduto = produto.nome
     document.title = `${nomeProduto} | Loja exemplo`
+
+
+
+// Funções
+
+function buy(){
+    if (!sizeSelected){
+        alert('Selecione um tamanho.')
+    } else {
+        alert(
+            produto.nome + produto.price + produto.colorString + sizeSelected
+        )
+    }
+}
--- src/js/script.js
+++ src/js/script.js
@@ -1,109 +1,109 @@
 const tenis = [
-    {
-        nome: 'Nike',
-        price: 599.90,
-        colorString: 'Branco',
-        color: '#eee',
-        sizes: [41, 43],
-        img: 'img/img_example.webp',
-    },
-    {
-        nome: 'Adidas',
-        price: 499.90,
-        colorString: 'Preto',
-        color: '#000000',
-        sizes: [38, 40, 42],
-        img: 'img/img_example.webp',
-    },
-    {
-        nome: 'Puma',
-        price: 389.50,
-        colorString: 'Azul',
-        color: '#0000FF',
-        sizes: [39, 41, 44],
-        img: 'img/img_example.webp',
-    },
-    {
-        nome: 'Vans',
-        price: 429.99,
-        colorString: 'Vermelho',
-        color: '#FF0000',
-        sizes: [38, 42, 45],
-        img: 'img/img_example.webp',
-    },
-    {
-        nome: 'Converse',
-        price: 379.90,
-        colorString: 'Cinza',
-        color: '#808080',
-        sizes: [40, 43],
-        img: 'img/img_example.webp',
-    },
-    {
-        nome: 'Asics',
-        price: 549.00,
-        colorString: 'Verde',
-        color: '#008000',
-        sizes: [39, 41, 44, 45],
-        img: 'img/img_example.webp',
-    },
-    {
-        nome: 'New Balance',
-        price: 469.99,
-        colorString: 'Amarelo',
-        color: '#FFFF00',
-        sizes: [38, 40, 42, 44],
-        img: 'img/img_example.webp',
-    },
-    {
-        nome: 'Fila',
-        price: 359.90,
-        colorString: 'Roxo',
-        color: '#800080',
-        sizes: [39, 41, 43],
-        img: 'img/img_example.webp',
-    },
-    {
-        nome: 'Reebok',
-        price: 419.00,
-        colorString: 'Laranja',
-        color: '#FFA500',
-        sizes: [38, 40, 42, 45],
-        img: 'img/img_example.webp',
-    }
-]
+  {
+    nome: "Nike",
+    price: 599.9,
+    colorString: "Branco",
+    color: "#eee",
+    sizes: [41, 43],
+    img: "img/img_example.webp",
+  },
+  {
+    nome: "Adidas",
+    price: 499.9,
+    colorString: "Preto",
+    color: "#000000",
+    sizes: [38, 40, 42],
+    img: "img/img_example.webp",
+  },
+  {
+    nome: "Puma",
+    price: 389.5,
+    colorString: "Azul",
+    color: "#0000FF",
+    sizes: [39, 41, 44],
+    img: "img/img_example.webp",
+  },
+  {
+    nome: "Vans",
+    price: 429.99,
+    colorString: "Vermelho",
+    color: "#FF0000",
+    sizes: [38, 42, 45],
+    img: "img/img_example.webp",
+  },
+  {
+    nome: "Converse",
+    price: 379.9,
+    colorString: "Cinza",
+    color: "#808080",
+    sizes: [40, 43],
+    img: "img/img_example.webp",
+  },
+  {
+    nome: "Asics",
+    price: 549.0,
+    colorString: "Verde",
+    color: "#008000",
+    sizes: [39, 41, 44, 45],
+    img: "img/img_example.webp",
+  },
+  {
+    nome: "New Balance",
+    price: 469.99,
+    colorString: "Amarelo",
+    color: "#FFFF00",
+    sizes: [38, 40, 42, 44],
+    img: "img/img_example.webp",
+  },
+  {
+    nome: "Fila",
+    price: 359.9,
+    colorString: "Roxo",
+    color: "#800080",
+    sizes: [39, 41, 43],
+    img: "img/img_example.webp",
+  },
+  {
+    nome: "Reebok",
+    price: 419.0,
+    colorString: "Laranja",
+    color: "#FFA500",
+    sizes: [38, 40, 42, 45],
+    img: "img/img_example.webp",
+  },
+];
 
-for (let i = 0; i < tenis.length; i++){
-    let box = document.createElement('li')
-    let img = document.createElement('img')
-    let nameBox = document.createElement('div')
-    let nameTenis = document.createElement('h2')
-    let price = document.createElement('h2')
-    let buyBtn = document.createElement('button')
-    
-    box.className = 'item-box'
-    img.className = 'item-img'
-    img.src = tenis[i].img
-    nameBox.classList = 'name-box'
-    nameTenis.className = 'item-name'
-    nameTenis.textContent = tenis[i].nome
-    price.className = 'price'
-    price.textContent = 'R$ ' + tenis[i].price.toFixed(2)
-    buyBtn.className = 'buy'
-    buyBtn.textContent = 'Comprar'
-    buyBtn.onclick = () => {
-        sessionStorage.setItem('productSelected', JSON.stringify(tenisSelecionado)) // Transforma o objeto em string e guarda na sessionStorage
-        window.open('buy_page.html')
-    }
+for (let i = 0; i < tenis.length; i++) {
+  let box = document.createElement("li");
+  let img = document.createElement("img");
+  let nameBox = document.createElement("div");
+  let nameTenis = document.createElement("h2");
+  let price = document.createElement("h2");
+  let buyBtn = document.createElement("button");
 
-    box.appendChild(img)
-    nameBox.appendChild(nameTenis)
-    nameBox.appendChild(price)
-    box.appendChild(nameBox)
-    box.appendChild(buyBtn)
+  box.className = "item-box";
+  img.className = "item-img";
+  img.src = tenis[i].img;
+  nameBox.classList = "name-box";
+  nameTenis.className = "item-name";
+  nameTenis.textContent = tenis[i].nome;
+  price.className = "price";
+  price.textContent = "R$ " + tenis[i].price.toFixed(2);
+  buyBtn.className = "buy";
+  buyBtn.textContent = "Comprar";
+  buyBtn.onclick = () => {
+    sessionStorage.setItem("productSelected", JSON.stringify(tenisSelecionado)); // Transforma o objeto em string e guarda na sessionStorage
+    window.open("product_page.html");
+  };
 
-    const list = document.getElementById('list')
-    list.appendChild(box)
+  box.appendChild(img);
+  nameBox.appendChild(nameTenis);
+  nameBox.appendChild(price);
+  box.appendChild(nameBox);
+  box.appendChild(buyBtn);
 
-    let tenisSelecionado = tenis[i]
+  const list = document.getElementById("list");
+  list.appendChild(box);
+
+  let tenisSelecionado = tenis[i];
 }
